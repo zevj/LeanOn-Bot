602d09c9835add8f185d9513d5394982fbf05616
Ira Jacob Javier <Jacobalvezjavier@gmail.com>
2026-05-06 09:29:29 +0800

    database user seeder

---
+++ backend/database/seeders/UsersTableSeeder.php
@@ -0,0 +1,65 @@
+<?php
+
+namespace Database\Seeders;
+
+use Illuminate\Database\Seeder;
+use Illuminate\Support\Facades\DB;
+use Illuminate\Support\Facades\Hash;
+use Carbon\Carbon;
+
+class UsersTableSeeder extends Seeder
+{
+    /**
+     * Run the database seeds.
+     */
+    public function run(): void
+    {
+        DB::table('users')->insert([
+            [
+                'first_name' => 'Guidance',
+                'last_name' => 'Officer',
+                'email' => 'guidance@gordoncollege.edu.ph',
+                'email_verified_at' => Carbon::now(),
+                'password' => Hash::make('123456'),
+                'role' => 'guidance',
+                'department' => null,
+                'program' => null,
+                'year_level' => null,
+                'phone_number' => null,
+                'terms_accepted_at' => Carbon::now(),
+                'created_at' => Carbon::now(),
+                'updated_at' => Carbon::now(),
+            ],
+            [
+                'first_name' => 'Ira Jacob',
+                'last_name' => 'Javier',
+                'email' => '202311473@gordoncollege.edu.ph',
+                'email_verified_at' => Carbon::now(),
+                'password' => Hash::make('123456'),
+                'role' => 'student',
+                'department' => 'CCS',
+                'program' => 'Bachelor of Science in Information Technology',
+                'year_level' => '3rd Year',
+                'phone_number' => null,
+                'terms_accepted_at' => Carbon::now(),
+                'created_at' => Carbon::now(),
+                'updated_at' => Carbon::now(),
+            ],
+            [
+                'first_name' => 'Allysa',
+                'last_name' => 'Lingad',
+                'email' => '202310636@gordoncollege.edu.ph',
+                'email_verified_at' => Carbon::now(),
+                'password' => Hash::make('123456'),
+                'role' => 'student',
+                'department' => 'CCS',
+                'program' => 'Bachelor of Science in Information Technology',
+                'year_level' => '3rd Year',
+                'phone_number' => null,
+                'terms_accepted_at' => Carbon::now(),
+                'created_at' => Carbon::now(),
+                'updated_at' => Carbon::now(),
+            ],
+        ]);
+    }
+}
