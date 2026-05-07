0f303ea7d579dad76792c9e71c12861e0f1235b0
Ira Jacob Javier <Jacobalvezjavier@gmail.com>
2026-05-07 12:14:32 +0800

    profile image

--- frontend/src/components/sidebarStudent.vue
+++ frontend/src/components/sidebarStudent.vue
@@ -121,7 +121,7 @@
       <div class="logout">
         <div class="picture-info-separation" @click="openModal">
           <div class="picture">
-            <img :src="userProfile.profile_image_url || '/leanOnBot.png'" class="logo-icon" style="object-fit: cover; width: 100%; height: 100%; border-radius: 50%;" />
+            <img :src="'/leanOnBot.png'"/>
           </div>
           <div class="title-footer">
             <span class="logo-text">{{ userProfile.first_name }} {{ userProfile.last_name }}</span>
